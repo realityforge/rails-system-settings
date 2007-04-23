8920bb72e6eeecdc6f56866f67bc4c7cd816784d
Peter Donald <peter@realityforge.org>
2007-04-23 23:51:04 +0000

    Update to reflect change in name of class

--- lib/system_setting.rb
+++ lib/system_setting.rb
@@ -1,11 +1,11 @@
 class SystemSetting < ActiveRecord::Base
    def self.[](name)
-      p = Property.find_by_name(name)
+      p = SystemSetting.find_by_name(name)
       p ? p.value : nil
    end
 
    def self.[]=(name,value)
-      p = Property.find_or_create_by_name(name)
+      p = SystemSetting.find_or_create_by_name(name)
       p.value = value
       p.save!
    end
