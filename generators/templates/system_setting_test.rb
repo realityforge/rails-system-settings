f99a51df974b4cc7fdc133563badf9473e2974b2
Peter Donald <peter@realityforge.org>
2007-04-24 00:32:21 +0000

    Fix test to work with sample fixture data

--- generators/templates/system_setting_test.rb
+++ generators/templates/system_setting_test.rb
@@ -2,21 +2,21 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class SystemSettingTest < Test::Unit::TestCase
    def test_basic_load
-      system_setting = system_settings(:p1)
+      system_setting = system_settings(:s_environment)
       assert_equal( 1, system_setting.id )
-      assert_equal( 'import_date', system_setting.name )
-      assert_equal( '6/2/2007 10:00am', system_setting.value )
+      assert_equal( 'environment', system_setting.name )
+      assert_equal( 'test', system_setting.value )
       assert_equal( true, system_setting.valid? )
    end
 
    def test_array_read
-      assert_equal( '6/2/2007 10:00am', SystemSetting['import_date'] )
+      assert_equal( 'test', SystemSetting['environment'] )
    end
 
    def test_array_write_for_existing
-      assert_equal( '6/2/2007 10:00am', SystemSetting['import_date'] )
-      SystemSetting['import_date'] = '42'
-      assert_equal( '42', SystemSetting['import_date'] )
+      assert_equal( 'test', SystemSetting['environment'] )
+      SystemSetting['environment'] = '42'
+      assert_equal( '42', SystemSetting['environment'] )
    end
 
    def test_array_write_for_new
