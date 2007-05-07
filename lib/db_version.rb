de2425d8069fddfe442b7e80d72ad74bb15c4e96
Peter Donald <peter@realityforge.org>
2007-05-07 03:12:54 +0000

    Rework plugin so can extract version in other mechanism other than helper

--- lib/db_version.rb
+++ lib/db_version.rb
@@ -2,19 +2,28 @@ module DbVersion
   def self.included(base)
     base.class_eval do
       helper_method :db_version
-      
+
       protected
-      
-      def db_version  
-        mv = SystemSetting['minor.version']
-        sv = schema_version
-        mv.nil? ? sv : "#{sv}.#{mv}"
+
+      def db_version
+        DbVersion.db_version
       end
 
-      def schema_version
-        (ActiveRecord::Base.connection.select_one("SELECT version FROM schema_info") || {"version" => 0})["version"]
-      end
     end
+  end
+
+  def self.db_version
+    mv = DbVersion.minor_version
+    sv = DbVersion.schema_version
+    mv.nil? ? sv : "#{sv}.#{mv}"
+  end
+
+  def self.minor_version
+    SystemSetting['minor.version']
+  end
+
+  def self.schema_version
+    (ActiveRecord::Base.connection.select_one("SELECT version FROM schema_info") || {"version" => 0})["version"]
   end
 end
 
