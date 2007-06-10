9a7d5b03c5ab5b7199c4881e35c3c310f90ab6a3
Peter Donald <peter@realityforge.org>
2007-06-10 23:12:30 +0000

    Manually specify validations

--- lib/system_setting.rb
+++ lib/system_setting.rb
@@ -1,13 +1,16 @@
 class SystemSetting < ActiveRecord::Base
-   def self.[](name)
-      p = SystemSetting.find_by_name(name)
-      p ? p.value : nil
-   end
+  validates_length_of :name, :in => 1..255
+  validates_uniqueness_of :name
 
-   def self.[]=(name,value)  
-      p = SystemSetting.find_by_name(name)
-      p = SystemSetting.new(:name => name) unless p 
-      p.value = value
-      p.save!
-   end
+  def self.[](name)
+    p = SystemSetting.find_by_name(name)
+    p ? p.value : nil
+  end
+
+  def self.[]=(name, value)
+    p = SystemSetting.find_by_name(name)
+    p = SystemSetting.new(:name => name) unless p
+    p.value = value
+    p.save!
+  end
 end
